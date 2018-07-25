049f88663527bf4c5bc15351f37a12d17f7e6b9e
Amstrad00 <hated.proud@gmail.com>
2018-07-25 14:43:52 -0400

    Update rps-restyler.user.js

--- rps-restyler.user.js
+++ rps-restyler.user.js
@@ -6,7 +6,7 @@
 // @author       Amstrad00
 // @match        https://www.rockpapershotgun.com/*
 // @grant        none
-// @run-at       document-start
+// @run-at       document-end
 // @updateURL    https://github.com/Amstrad00/rps-restyler-darker/raw/master/rps-restyler.user.js
 // @downloadURL  https://github.com/Amstrad00/rps-restyler-darker/raw/master/rps-restyler.user.js
 // ==/UserScript==
